787a51f0f6961af509b7d995c4999a74f3047787
juein <apocriz@gmail.com>
2020-01-22 14:33:09 +0900

    스타일 컴포넌트 기본 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 function App() {
   return (
-    <>
-    <div className="App">test</div>
-    </>
+    <Container>
+      <Button green>green</Button>
+      <Button className="fs20">pink</Button>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  background: #ffeaa7;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  border: 0;
+  outline: none;
+  background: ${props => props.green ? '#00cec9' : '#ff7675'};
+  &.fs20{
+    font-size: 20px;
+  }
+  
+`;
 
 export default App;
