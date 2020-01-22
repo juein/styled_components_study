54106aefcbb3cd335e2ddb4873ffac7da96da071
juein <apocriz@gmail.com>
2020-01-22 14:53:54 +0900

    injectGlobal, createGlobalStyle (공통 스타일 적용)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle  } from 'styled-components';
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <Container>
       <Button green>green</Button>
       <Button className="fs20">pink</Button>
     </Container>
+    </>
   );
 }
+
+// styled components는 해당 컴포넌트에만 스타일이 적용된다.
+// injectGlobal 사용시 공통으로 적용될 스타일을 지정할 수 있다.
+// v4부터 injectGlobal이 createGlobalStyle 로 대체되었다.
+const GlobalStyle  = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const Container = styled.div`
   background: #ffeaa7;
